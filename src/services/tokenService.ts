cfe2734d394c081c30c7430658f8844eb906e508
Luiggi Lamela <stefano.lamelav@gmail.com>
2023-08-11 14:38:15 -0500

    update documentation bd

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,9 +1,4 @@
-import * as redis from 'redis';
-import { promisify } from 'util';
-
-const client = redis.createClient();
-const setAsync = promisify(client.set).bind(client);
-const getAsync = promisify(client.get).bind(client);
+import { query } from '../services/db'; 
 
 class TokenService {
   private static EXPIRATION_SECONDS = 900;  // 15 minutos
@@ -16,31 +11,37 @@ class TokenService {
   }
 
   /**
-   * Tokeniza los datos de la tarjeta y los guarda en Redis.
+   * Tokeniza los datos de la tarjeta y los guarda en la base de datos.
    * @param cardData Datos de la tarjeta.
    * @returns Token generado.
    */
   public async tokenize(cardData: any): Promise<string> {
     const token = this.generateUniqueToken();
-    await setAsync(token, JSON.stringify(cardData), 'EX', TokenService.EXPIRATION_SECONDS);
+    await query(
+      `INSERT INTO tokenized_cards (token, card_number, cvv, expiration_month, expiration_year, email)
+       VALUES ($1, $2, $3, $4, $5, $6)`,
+      [token, cardData.cardNumber, cardData.cvv, cardData.expirationMonth, cardData.expirationYear, cardData.email]
+    );
     return token;
   }
 
   /**
-   * Obtiene los datos de la tarjeta de Redis usando su token.
+   * Obtiene los datos de la tarjeta de la base de datos usando su token.
    * @param token Token a buscar.
    * @returns Datos de la tarjeta.
    * @throws Error si el token no se encuentra.
    */
   public async fetchCardByToken(token: string): Promise<any> {
-    const cardDataString = await getAsync(token);
-    if (!cardDataString) {
+    const result = await query(
+      `SELECT card_number, expiration_month, expiration_year, email
+       FROM tokenized_cards
+       WHERE token = $1 AND CURRENT_TIMESTAMP < expiration`,
+      [token]
+    );
+    if (result.rows.length === 0) {
       throw new Error('Token no encontrado');
     }
-
-    const cardData = JSON.parse(cardDataString);
-    delete cardData.cvv;  // Es esencial no devolver el CVV por razones de seguridad.
-    return cardData;
+    return result.rows[0];
   }
 }
 
